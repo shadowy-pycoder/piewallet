81696236d5eb1664a9919539308e3f5adf870a0f
shadowy-pycoder <shadowy-pycoder@example.com>
2023-01-20 08:42:00 +0300

    added message signing/verification

--- src/piewallet/piewallet.py
+++ src/piewallet/piewallet.py
@@ -1,4 +1,6 @@
+import base64
 from secrets import randbelow
+import sys
 from time import perf_counter
 
 import base58
@@ -7,6 +9,7 @@ import bech32  # type: ignore
 from curve_params import secp256k1, JacobianPoint, Point, Signature, IDENTITY_POINT, POW_2_256_M1
 from functions.double_sha256 import double_sha256
 from functions.ripemd160_sha256 import ripemd160_sha256
+from functions.sha256 import sha256
 
 
 class PieWalletException(Exception):
@@ -19,6 +22,10 @@ class PrivateKeyError(PieWalletException):
 
 class PointError(PieWalletException):
     '''Point is not on an elliptic curve'''
+
+
+class SignatureError(PieWalletException):
+    '''Invalid ECDSA signature parameters'''
 
 
 class PrivateKey:
@@ -57,6 +64,7 @@ class PrivateKey:
         return f'{cls_name}({key[:4]}...{key[-4:]}, uncompressed={self.uncompressed})'
 
     def _generate(self) -> int:
+        '''Generates cryptographically-secure random integer'''
         return randbelow(secp256k1.n_curve)
 
     @staticmethod
@@ -65,7 +73,7 @@ class PrivateKey:
         return isinstance(scalar, int) and not (scalar <= 0 or scalar >= secp256k1.n_curve)
 
     @staticmethod
-    def valid_checksum(version: bytes, privkey: bytes, checksum: bytes) -> bool:
+    def valid_checksum(version: bytes, privkey: bytes, checksum: bytes, /) -> bool:
         return double_sha256(version + privkey)[:4] == checksum
 
     @staticmethod
@@ -109,9 +117,15 @@ class PrivateKey:
 class PublicKey(PrivateKey):
 
     __precomputes: list[JacobianPoint] = []
+    __headers = [[b'\x1b', b'\x1c', b'\x1d', b'\x1e'],
+                 [b'\x1f', b'\x1f', b'\x20', b'\x22'],
+                 [b'\x23', b'\x24', b'\x25', b'\x26'],
+                 [b'\x27', b'\x29', b'\x28', b'\x2a'],
+                 [b'\x2b', b'\x2c', b'\x2d', b'\x2e']]
 
     def __init__(self, privkey: int | None = None, /, *, uncompressed: bool = False) -> None:
         super().__init__(privkey, uncompressed=uncompressed)
+        self.__raw_public_key: Point | None = None
         self.__public_key: bytes | None = None
         self.__address: str | None = None
         self.__nested_segwit_address: str | None = None
@@ -149,10 +163,16 @@ class PublicKey(PrivateKey):
         return self.__native_segwit_address
 
     @property
+    def raw_public_key(self) -> Point:
+        if self.__raw_public_key is None:
+            self.__raw_public_key = self._raw_pubkey()
+        return self.__raw_public_key
+
+    @property
     def public_key_bytes(self) -> bytes:
         '''Returns public key in bytes format'''
         if self.__public_key is None:
-            self.__public_key = self._create_pubkey(uncompressed=self.uncompressed)
+            self.__public_key = self._create_pubkey(self.raw_public_key, uncompressed=self.uncompressed)
         return self.__public_key
 
     @property
@@ -160,7 +180,7 @@ class PublicKey(PrivateKey):
         '''Returns public key in HEX format'''
         return f'{self.public_key_bytes.hex()}'
 
-    def _ec_dbl(self, q: JacobianPoint) -> JacobianPoint:
+    def _ec_dbl(self, q: JacobianPoint, /) -> JacobianPoint:
         # Fast Prime Field Elliptic Curve Cryptography with 256 Bit Primes
         # Shay Gueron, Vlad Krasnov
         # https://eprint.iacr.org/2013/816.pdf page 4
@@ -174,7 +194,7 @@ class PublicKey(PrivateKey):
         z = (2 * q.y * q.z) % secp256k1.p_curve
         return JacobianPoint(x, y, z)
 
-    def _ec_add(self, p: JacobianPoint, q: JacobianPoint) -> JacobianPoint:
+    def _ec_add(self, p: JacobianPoint, q: JacobianPoint, /) -> JacobianPoint:
         # Fast Prime Field Elliptic Curve Cryptography with 256 Bit Primes
         # Shay Gueron, Vlad Krasnov
         # https://eprint.iacr.org/2013/816.pdf page 4
@@ -211,7 +231,7 @@ class PublicKey(PrivateKey):
             PublicKey.__precomputes.append(dbl)
             dbl = self._ec_dbl(dbl)
 
-    def _ec_mul(self, scalar: int, point: Point | JacobianPoint | None = None) -> JacobianPoint:
+    def _ec_mul(self, scalar: int, point: Point | JacobianPoint | None = None, /) -> JacobianPoint:
         # https://paulmillr.com/posts/noble-secp256k1-fast-ecc/#fighting-timing-attacks
         n = scalar
         p = IDENTITY_POINT
@@ -237,34 +257,39 @@ class PublicKey(PrivateKey):
                 q = self._ec_dbl(q)
         return JacobianPoint(p.x, p.y, p.z)
 
-    def _create_pubkey(self, *, uncompressed: bool = False) -> bytes:
-        raw_pubkey: Point = self.to_affine(self._ec_mul(self.private_key))
+    def _raw_pubkey(self) -> Point:
+        raw_pubkey = self.to_affine(self._ec_mul(self.private_key))
         if not self.valid_point(raw_pubkey):
             raise PointError('Point is not on curve')
+        return raw_pubkey
 
+    def _create_pubkey(self, raw_pubkey: Point, /, *, uncompressed: bool = False) -> bytes:
         if uncompressed:
             return b'\x04' + raw_pubkey.x.to_bytes(32, 'big') + raw_pubkey.y.to_bytes(32, 'big')
 
-        prefix = b'\x03' if raw_pubkey.y & 1 else b'\x02'
+        prefix = b'\x03' if self._is_odd(raw_pubkey.y) else b'\x02'
         return prefix + raw_pubkey.x.to_bytes(32, 'big')
 
-    def _create_address(self, pubkey: bytes) -> str:
+    def _create_address(self, pubkey: bytes, /) -> str:
         address = b'\x00' + ripemd160_sha256(pubkey)
         return base58.b58encode_check(address).decode('UTF-8')
 
-    def _create_nested_segwit(self, pubkey: bytes) -> str:
+    def _create_nested_segwit(self, pubkey: bytes, /) -> str:
         address = b'\x05' + ripemd160_sha256(b'\x00\x14' + ripemd160_sha256(pubkey))
         return base58.b58encode_check(address).decode('UTF-8')
 
-    def _create_native_segwit(self, pubkey: bytes) -> str:
+    def _create_native_segwit(self, pubkey: bytes, /) -> str:
         return bech32.encode('bc', 0x00, ripemd160_sha256(pubkey))
 
+    def _is_odd(self, n: int) -> bool:
+        return bool(n & 1)
+
     @staticmethod
-    def mod_inverse(n: int, mod: int) -> int:
+    def mod_inverse(n: int, /, mod: int) -> int:
         return pow(n, -1, mod)
 
     @staticmethod
-    def to_affine(p: JacobianPoint) -> Point:
+    def to_affine(p: JacobianPoint, /) -> Point:
         '''Converts jacobian point to affine point'''
         inv_z = PublicKey.mod_inverse(p.z, secp256k1.p_curve)
         inv_z2 = inv_z ** 2
@@ -273,7 +298,7 @@ class PublicKey(PrivateKey):
         return Point(x, y)
 
     @staticmethod
-    def to_jacobian(p: Point) -> JacobianPoint:
+    def to_jacobian(p: Point, /) -> JacobianPoint:
         '''Converts affine point to jacobian point'''
         return JacobianPoint(p.x, p.y, z=1)
 
@@ -286,101 +311,136 @@ class PublicKey(PrivateKey):
         except (TypeError, IndexError):  # Exception is raised when given arguments are invalid (non-integers)
             return False  # which also means point is not on curve
 
-    def sign(self, privkey: int, mhash: int, nonce: int | None = None) -> Signature:
-        if nonce is None:
-            while True:
-                nonce = self._generate()
-                if self.valid_key(nonce):
-                    break
-        r = self.to_affine(self._ec_mul(nonce)).x % secp256k1.n_curve
-        s = (self.mod_inverse(nonce, secp256k1.n_curve) * (mhash + privkey * r)) % secp256k1.n_curve
+    def msg_magic(self, message: str) -> str:
+        return "\x18Bitcoin Signed Message:\n" + chr(len(message)) + message
+
+    def _sign(self, privkey: int, mhash: int, /) -> Signature:
+        # https://learnmeabitcoin.com/technical/ecdsa#sign
+        while True:
+            k = self._generate()
+            if not self.valid_key(k):
+                continue
+            # when working with private keys, standard multiplication is used
+            point = self.to_affine(self._ec_mul(k))
+            r = point.x % secp256k1.n_curve
+            if r == 0 or point == IDENTITY_POINT:
+                continue
+            s = self.mod_inverse(k, secp256k1.n_curve) * (mhash + privkey * r) % secp256k1.n_curve
+            if s == 0:
+                continue
+            if s > secp256k1.n_curve // 2:  # https://github.com/bitcoin/bips/blob/master/bip-0062.mediawiki
+                s = secp256k1.n_curve - s
+            break
         return Signature(r, s)
 
-    def verify(self, pubkey: Point, sig: Signature, mhash: int) -> bool:
-        pt1 = self._ec_mul(self.mod_inverse(sig.s, secp256k1.n_curve) * mhash, secp256k1.gen_point)
-        pt2 = self._ec_mul(self.mod_inverse(sig.s, secp256k1.n_curve) * sig.r, pubkey)
-        pt3 = self.to_affine(self._ec_add(pt1, pt2))
-        return pt3.x == sig.r
+    def _verify(self, pubkey: Point, sig: Signature, mhash: int, /) -> bool:
+        # https://learnmeabitcoin.com/technical/ecdsa#verify
+        # when working with public keys, unsafe multiplication is used
+        p = self._ec_mul(self.mod_inverse(sig.s, secp256k1.n_curve) * mhash, secp256k1.gen_point)
+        q = self._ec_mul(self.mod_inverse(sig.s, secp256k1.n_curve) * sig.r, pubkey)
+        pq = self.to_affine(self._ec_add(p, q))
+        return pq.x == sig.r
+
+    def _sign_message(self, address: str, message: str) -> str:
+        m_bytes = self.msg_magic(message).encode('ascii')
+        m_hash = int.from_bytes(double_sha256(m_bytes), 'big')
+        sig = self._sign(self.private_key, m_hash)
+
+        if address.startswith('bc'):
+            ver = 3
+        else:
+            if self.uncompressed:
+                ver = 0
+            else:
+                addr_bytes = base58.b58decode_check(address)
+                if addr_bytes.startswith(b'\x00'):
+                    ver = 1
+                else:
+                    ver = 2
+        r = sig.r.to_bytes(32, 'big')
+        s = sig.s.to_bytes(32, 'big')
+        for header in self.__headers[ver]:
+            sig = base64.b64encode(header + r + s).decode('ascii')
+            verified = self.verify_message(address, message, sig)
+            if verified:
+                return sig
+
+    def bitcoin_message(self, address: str, message: str) -> None:
+        print('-----BEGIN BITCOIN SIGNED MESSAGE-----')
+        print(message)
+        print('-----BEGIN BITCOIN SIGNATURE-----')
+        print(address)
+        print()
+        print(self._sign_message(address, message))
+        print('-----END BITCOIN SIGNATURE-----')
+
+    def verify_message(self, address: str, message: str, sig: str):
+        dsig = base64.b64decode(sig)
+        if len(dsig) != 65:
+            raise SignatureError("Signature must be 65 bytes long:", len(dsig))
+        ver = dsig[:1]
+        m_bytes = self.msg_magic(message).encode('ascii')
+        z = int.from_bytes(double_sha256(m_bytes), 'big')
+        header, r, s = dsig[0], int.from_bytes(dsig[1:33], 'big'), int.from_bytes(dsig[33:], 'big')
+        if header < 27 or header > 42:
+            raise SignatureError("Header byte out of range:", header)
+        if header >= 39:
+            header -= 12
+        elif header >= 35:
+            header -= 8
+        elif header >= 31:
+            header -= 4
+        recid = header - 27
+        x = r + secp256k1.n_curve * (recid // 2)
+        alpha = (pow(x, 3) + secp256k1.b_curve) % secp256k1.p_curve
+        beta = pow(alpha, (secp256k1.p_curve + 1) // 4, secp256k1.p_curve)
+        if (beta - recid) % 2 == 0:
+            y = beta
+        else:
+            y = secp256k1.p_curve - beta
+        R = Point(x, y)
+        e = (-z) % secp256k1.n_curve
+        inv_r = self.mod_inverse(r, secp256k1.n_curve)
+        p = self._ec_mul(s, R)
+        q = self._ec_mul(e, secp256k1.gen_point)
+        Q = self._ec_add(p, q)
+        raw_pubkey = self.to_affine(self._ec_mul(inv_r, Q))
+        if ver in self.__headers[0]:
+            pubkey = self._create_pubkey(raw_pubkey, uncompressed=True)
+            addr = self._create_address(pubkey)
+        elif ver in self.__headers[1]:
+            pubkey = self._create_pubkey(raw_pubkey)
+            addr = self._create_address(pubkey)
+        elif ver in self.__headers[2]:
+            pubkey = self._create_pubkey(raw_pubkey)
+            addr = self._create_nested_segwit(pubkey)
+        elif ver in self.__headers[3]:
+            pubkey = self._create_pubkey(raw_pubkey)
+            addr = self._create_native_segwit(pubkey)
+        elif ver in self.__headers[4]:
+            raise NotImplementedError()
+        else:
+            raise SignatureError("Header byte out of range:", header)
+        return addr == address
 
 
-class Address(PublicKey):
-    def _create_address(self, pubkey: bytes) -> str:
-        print('Creating address...')
-        address = b'\x00' + ripemd160_sha256(pubkey)
-        return base58.b58encode_check(address).decode('UTF-8')
+class PieWallet(PublicKey):
+    def __init__(self):
+        super().__init__()
 
 
 if __name__ == '__main__':
-    # print(_ec_mul(0xFF) == (12312385769684547396095365029355369071957339694349689622296638024179682296192,
-    #       29045073188889159330506972844502087256824914692696728592611344825524969277689))
-    # print(_ec_mul(0xEE31862668ECD0EC1B3538B04FBF21A59965B51C5648F5CE97C613B48610FA7B) == (
-    #     49414738088508426605940350615969154033259972709128027173379136589046972286596, 113066049041265251152881802696276066009952852537138792323892337668336798103501))
-    my_key = PublicKey(0xFF, uncompressed=False)
-    # assert my_key.public_key == '031B38903A43F7F114ED4500B4EAC7083FDEFECE1CF29C63528D563446F972C180'.lower()
-    # assert my_key.public_key == '041B38903A43F7F114ED4500B4EAC7083FDEFECE1CF29C63528D563446F972C1804036EDC931A60AE889353F77FD53DE4A2708B26B6F5DA72AD3394119DAF408F9'.lower()
-    print(my_key.public_key)
-    print(my_key._PublicKey__precomputes[1])
+    my_key = PublicKey(112757557418114203588093402336452206775565751179231977388358956335153294300646)
     print(my_key.wif_private_key)
-    print(PrivateKey.to_wif(0xFF, uncompressed=True))
-
-    print(my_key.native_segwit_address)
-    print(my_key.nested_segwit_address)
-    print(my_key.address)
-    my_privkey = PrivateKey(0xFF)
-    print(my_privkey.wif_private_key)
-    print(PrivateKey.valid_key(0xC0FEE))
-    b = (12312385769684547396095365029355369071957339694349689622296638024179682296192,
-         29045073188889159330506972844502087256824914692696728592611344825524969277689)
-    print(my_key.valid_point(b))
-    c = Point(x=12312385769684547396095365029355369071957339694349689622296638024179682296192,
-              y=29045073188889159330506972844502087256824914692696728592611344825524969277689)
-    print(my_key.valid_point((c.x, c.y)))
-    # print(getattr(my_key, 'address', []))
-    # print(hasattr(my_key, 'generate'))
-    # try:
-    #     setattr(my_key, 'private_key', 'hack_bitcoin')
-    # except AttributeError:
-    #     print('Sorry, can\'t hack bitcoin')
-    # print(vars(my_key))
-    # print(vars())
-    # print([(key, value) for key, value in my_key.__dict__.items()])
-    my_key2 = PublicKey(0xFF, uncompressed=True)
-    print(my_key2.hex_private_key)
-    print(my_key2.private_key)
-    print(my_key2.wif_private_key)
-    print(my_key2.public_key)
-    print(my_key2.native_segwit_address)
-    print(my_key2)
-    my_key3 = Address(0xFF)
-    print(my_key3.address)
-    print(my_key3.wif_private_key)
-    print(my_key3._generate())
-    mes_hash = 103318048148376957923607078689899464500752411597387986125144636642406244063093
-    signature = Signature(108607064596551879580190606910245687803607295064141551927605737287325610911759,
-                          73791001770378044883749956175832052998232581925633570497458784569540878807131)
-    public_key = Point(x=33886286099813419182054595252042348742146950914608322024530631065951421850289,
-                       y=9529752953487881233694078263953407116222499632359298014255097182349749987176)
-    privkey = 112757557418114203588093402336452206775565751179231977388358956335153294300646
+    privkey = my_key.private_key
+    pubkey = my_key.raw_public_key
+    message = 'ECDSA is the most fun I have ever experienced'
+    m_hash = int.from_bytes(sha256(message.encode('ascii')), 'big')
     nonce = 12345
-    assert PublicKey().sign(privkey, mes_hash, nonce) == signature
-    # print(PublicKey().verify(public_key, signature, mes_hash))
-    assert PublicKey()._ec_mul(privkey) == PublicKey()._ec_mul(
-        privkey, point=Point(secp256k1.gen_point.x, secp256k1.gen_point.y))
-    jac = PublicKey().to_jacobian(public_key)
-    assert PublicKey()._ec_mul(privkey, public_key) == PublicKey()._ec_mul(
-        privkey, point=jac)
-    print(PublicKey().verify(public_key, signature, mes_hash))
-
-    print(PublicKey().verify(public_key, signature, mes_hash))
-    point = PublicKey()._ec_mul(0xFF, public_key)
-    print(PublicKey().to_affine(point))
-    b = JacobianPoint(x=12312385769684547396095365029355369071957339694349689622296638024179682296192,
-                      y=29045073188889159330506972844502087256824914692696728592611344825524969277689, z=1)
-    d = JacobianPoint(x=110131118690510186908950543931370188990156751066202461711764184129869302266467,
-                      y=18099170156017716056685675377519909261954775707007732472256775903887794233045, z=1)
-    e = JacobianPoint(x=56576513649176532955305617254616790498672209379484940581393603843805619269570,
-                      y=39155707150128334349216371677407456506802956851096117747929288260567018884059, z=65341020041517633956166170261014086368942546761318486551877808671514674964848)
-    e2 = PublicKey().to_affine(e)
-    print(PublicKey().to_affine(PublicKey()._ec_add(d, e)))
-
-    print(PublicKey().verify(public_key, signature, mes_hash))
+    address = my_key.native_segwit_address
+    sig, odd = my_key._sign(privkey, m_hash)
+    sig_real = 'G0PUXl2I51DzDEkTZJqWZZFf67X8ipazXLLgLhW28PYrFHMhsVrDzuDJtLbAcMUDFrz5uu7XSdXghDdIVw0P/W0='
+    decoded = base64.b64decode(sig_real).hex()
+    print(int(decoded[:2], 16), int(decoded[2:66], 16), int(decoded[66:], 16))
+    print(my_key.verify_message(address, message, sig_real))
+    my_key.bitcoin_message(address, message)
