83dbb491f95064c0a034bb2e99d61e447ca0b98d
shadowy-pycoder <shadowy-pycoder@example.com>
2023-01-25 20:42:45 +0300

    get rid of functions directory

--- src/piewallet/functions/double_sha256.py
+++
@@ -1,5 +0,0 @@
-from functions.sha256 import sha256
-
-
-def double_sha256(b: bytes) -> bytes:
-    return sha256(sha256(b))
--- src/piewallet/functions/ripemd160_sha256.py
+++
@@ -1,6 +0,0 @@
-from functions.sha256 import sha256
-from functions.ripemd160 import ripemd160
-
-
-def ripemd160_sha256(b: bytes) -> bytes:
-    return ripemd160(sha256(b))
